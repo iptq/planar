79cdda561e7608d7a4206736e33302bf36956604
Michael Zhang <iptq@protonmail.com>
2019-02-03 13:04:30 -0600

    aspect ratio

--- main.py
+++ main.py
@@ -13,11 +13,10 @@ from planar.states.editor import EditorState
 if __name__ == "__main__":
     pygame.init()
 
-    start_state = MenuState()
+    game = Game()
+    game.push_state(MenuState())
     if len(sys.argv) > 1:
         if sys.argv[1] == "editor":
-            start_state = EditorState((5, 5))
-    game = Game()
-    game.push_state(start_state)
+            game.push_state(EditorState((5, 5)))
 
     game.run()
