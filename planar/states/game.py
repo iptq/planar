ab5c41159cabe622e70ff37490a03534c307b90b
Michael Zhang <iptq@protonmail.com>
2019-02-02 23:48:47 -0600

    ...

--- planar/states/game.py
+++ planar/states/game.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame import Color
 
+import planar.constants as constants
 import planar.states as states
 
 class GameState(states.State):
@@ -15,6 +16,11 @@ class GameState(states.State):
 
     def draw(self, screen):
         screen.fill(Color(100, 80, 100))
-        left, right = self.level.render(30)
-        screen.blit(left, (0, 0))
-        screen.blit(right, (300, 0))
+
+        scale = constants.SCREEN_WIDTH // (2 * self.level.dim[0] + 6)
+        left, right = self.level.render(scale)
+
+        yoff = constants.SCREEN_HEIGHT - (self.level.dim[1] + 4) * scale
+
+        screen.blit(left, (2 * scale, yoff + 2 * scale))
+        screen.blit(right, ((4 + self.level.dim[0]) * scale, yoff + 2 * scale))
--- planar/states/levels.py
+++ planar/states/levels.py
@@ -3,7 +3,7 @@ from pygame import Color
 
 import planar.states as states
 from planar.states.game import GameState
-from planar.level import Level
+from planar.level import Level, Block, Segment, Player
 
 class LevelSelectState(states.State):
     def update(self, events):
@@ -12,7 +12,15 @@ class LevelSelectState(states.State):
                 if event.key == pygame.K_ESCAPE:
                     self.game.pop_state()
                 elif event.key == pygame.K_1:
-                    level = Level((5, 5), [], [])
+                    level = Level((10, 10), [
+                        Block([
+                            Segment(0, 0, 0, 1),
+                            Segment(0, 1, 0, 0)
+                        ], True, 0, [255, 10, 100])
+                    ], [
+                        Player(5, 5, 0, [66, 134, 244]),
+                        Player(5, 5, 1, [244, 83, 65])
+                    ])
                     self.game.push_state(GameState(level))
 
     def draw(self, screen):
