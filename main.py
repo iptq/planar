86f2d5b3246a76939ffcf48865cb153f11c59ae3
ashi6 <ashi6@illinois.edu>
2019-02-03 12:01:27 -0600

    more stuff

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import pygame
 import sys
 
 from planar import Game
-from planar.states.game import GameState
+from planar.states.menu import MenuState
 from planar.tests import *
 from planar.states.editor import EditorState
 
 if __name__ == "__main__":
     pygame.init()
-
-    menu_state = EditorState((5,5))
+    # menu_state = EditorState((5,5))
+    menu_state = MenuState()
     game = Game()
     game.push_state(menu_state)
 
