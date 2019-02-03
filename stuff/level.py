08061566ace0af3eb1f732046604fea3c3459a78
ashi6 <ashi6@illinois.edu>
2019-02-02 21:24:01 -0600

    spaces

--- stuff/level.py
+++ stuff/level.py
@@ -2,24 +2,25 @@ DIRECTION_HORIZONTAL = 0
 DIRECTION_VERTICAL = 1
 
 class Cell(object):
-	def __init__(self, x, y, t):
-		self.x = x
-		self.y = y
+    def __init__(self, x, y, z, t):
+        self.x = x
+        self.y = y
+        self.z = z
 
-		# t is the type of the block
-		# 0 = Full block
-		#  /\
-		# /21\
-		# \34/
-		#  \/
-		self.t = t
+        # t is the type of the block
+        # 0 = Full block
+        #  /\
+        # /21\
+        # \34/
+        #  \/
+        self.t = t
 
 class Block(object):
-	def __init__(self, cells, movable, direction):
-		self.cells = cells
-		self.movable = movable
-		self.direction = direction
+    def __init__(self, cells, movable, direction):
+        self.cells = cells
+        self.movable = movable
+        self.direction = direction
 
 class Level(object):
-	def __init__(self, objects):
-		pass
+    def __init__(self, objects):
+        pass
